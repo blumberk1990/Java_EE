1d7a518ba36b26181f20e3f5312eee5e5fb0968e
blumberk1990 <magda.hubert.magda@gmail.com>
2020-05-11 10:31:49 +0200

    homePage

---
+++ wykopek/02_program/src/pl/mgd/wykopek/controller/HomeController.java
@@ -0,0 +1,46 @@
+package pl.mgd.wykopek.controller;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pl.mgd.wykopek.model.Discovery;
+import pl.mgd.wykopek.service.DiscoveryService;
+
+/**
+ * Servlet implementation class HomeController
+ */
+@WebServlet("")
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		saveDiscoveriesInRequest(request);
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	private void saveDiscoveriesInRequest(HttpServletRequest request) {
+		DiscoveryService discoveryService = new DiscoveryService();
+		List<Discovery> allDiscoveries= discoveryService.getAllDiscoveries(new Comparator<Discovery>() {
+			//number of votes
+			@Override
+			public int compare(Discovery o1, Discovery o2) {
+				int o1Vote = o1.getUpVote() - o1.getDownVote();
+				int o2Vote = o2.getUpVote() - o2.getDownVote();
+				if(o1Vote < o2Vote) {
+					return 1;
+				} else if(o1Vote > o2Vote) {
+					return -1;
+				}
+				return 0;
+			}
+		});	
+		request.setAttribute("discoveries", allDiscoveries);
+	}
+}
